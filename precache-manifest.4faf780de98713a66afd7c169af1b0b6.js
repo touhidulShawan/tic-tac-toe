c6a461bf246b1ab9fdc4262818779cdfd4c14de8
Touhidul Shawan <touhidulshawan@gmail.com>
2020-06-30 21:04:01 +0600

    Updates

---
+++ precache-manifest.4faf780de98713a66afd7c169af1b0b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7552df67b3922b96352c501d6a8b182",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d20e839b2b3e7d3234a7",
+    "url": "/tic-tac-toe/static/css/main.e5876c3d.chunk.css"
+  },
+  {
+    "revision": "0f846664588a90d57533",
+    "url": "/tic-tac-toe/static/js/2.1e209bcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.1e209bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d20e839b2b3e7d3234a7",
+    "url": "/tic-tac-toe/static/js/main.80d1debb.chunk.js"
+  },
+  {
+    "revision": "61109fc60463736a7ada",
+    "url": "/tic-tac-toe/static/js/runtime-main.12138588.js"
+  }
+]);
